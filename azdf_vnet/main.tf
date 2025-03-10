8e5cfa00140893ff99689bb70adbbd02710b339c
Dvi23 <deveshlaljit@gmail.com>
2025-03-10 12:03:38 +0000

    Update main.tf

--- azdf_vnet/main.tf
+++ azdf_vnet/main.tf
@@ -21,5 +21,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.subnet_address_prefix]
+  address_prefix       = var.subnet_address_prefix
 }
